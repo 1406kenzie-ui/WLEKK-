ae8d236a5610f9be1032fe2ba4017744f5565c0d
Kenskiee <1406kenzie@gmail.com>
2025-12-26 00:51:09 +0700

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,31 @@
+const ROOMS = {
+  pintu: {
+    title: "Hai Kamu 🤍",
+    text: "Aku membuat tempat kecil ini, khusus untukmu.",
+    music: "assets/music/pintumasuk_lagu.mp3",
+    button: "Masuk"
+  },
+
+  tamu: {
+    title: "Ruang Tamu",
+    text: "Di sini aku ingin bercerita tentangmu.",
+    music: "assets/music/ruangtamu_lagu.mp3",
+    button: "Lanjut"
+  },
+
+  tengah: {
+    title: "Ruang Tengah",
+    text: "Setiap foto punya ceritanya sendiri.",
+    music: "assets/music/ruangtengah_lagu.mp3",
+    button: "Masuk Lebih Dalam"
+  },
+
+  utama: {
+    title: "Ruang Utama",
+    text: "Ini adalah perasaanku yang paling dalam.",
+    music: "assets/music/ruangutama_lagu.mp3",
+    button: "🤍"
+  }
+};
+
+const ROOM_ORDER = ["pintu", "tamu", "tengah", "utama"];
